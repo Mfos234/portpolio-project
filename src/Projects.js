8be4e0587e7909074374ac4eef5eec512767d0fd
Mike F <92463194+Mfos234@users.noreply.github.com>
2023-05-04 21:38:20 -0400

    Create Projects.js

---
+++ src/Projects.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+export default function Projects() {
+  return (
+    <div className="App">
+      <h1>Projects</h1>
+      <div class="project-container">
+        <div class="project-description">
+          <p>🚧 Under construction 🚧 </p>
+          <h1>List of Upcoming Projects</h1>
+          <p>Smart Mirror </p>
+          <p>Smart Home Dashboard </p>
+          <p>AR Pokedex </p>
+          <p>Smart Skateboard </p>
+        </div>
+      </div>
+    </div>
+  );
+}
