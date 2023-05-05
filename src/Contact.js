e1c1cad2f99f527ad087c3e29a22dffaeaa56c38
Mike F <92463194+Mfos234@users.noreply.github.com>
2023-05-04 21:51:13 -0400

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me!</h1>
+      <p>
+        If you are interested in working with me or have any questions, please
+        feel free to contact me. You can reach me by <a href="mailto:michaelfoster.dev@gmail.com">e-mail</a>. I look forward to
+        hearing from you soon.
+      </p>
+    </div>
+  );
+}
